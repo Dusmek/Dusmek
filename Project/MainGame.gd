b5a0c8635f7dce380535f51eb52ebc59ee464e0c
Dusmek <45102209+Dusmek@users.noreply.github.com>
2018-12-03 02:07:28 +0100

    vewport resolutions

--- Project/MainGame.gd
+++ Project/MainGame.gd
@@ -10,11 +10,23 @@ var CredModeClass = load("res://Credits.tscn")
 var MenuMode
 var GameMode
 var CredMode
+var GameScale
 #to game mode będzie odpowiedzialny za odpalanie odpowiedniej mapy: będzie je sobie wczytywał odpowiednio
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	var actualW = get_viewport().size.x
+	var actualH = get_viewport().size.y
+	var sceneW = ProjectSettings.get_setting("display/window/size/width")
+	var sceneH = ProjectSettings.get_setting("display/window/size/height")
+	if (sceneW/sceneH > actualW/actualH):
+		GameScale = sceneW/actualW
+	else:
+		GameScale = sceneH/actualH
+	
+	find_node("Camera2DX").zoom.x = GameScale;
+	find_node("Camera2DX").zoom.y = GameScale;
 	_showMenu()
 	pass
 
